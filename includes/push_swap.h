7a03ece7de0005d6f619aff057dc708df8cf4853
paul <paul@DESKTOP-AE7GCBC>
2025-01-28 01:13:15 +0100

    + | Modified header file, and did the norminette.

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -17,7 +17,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-typedef	struct	s_pile
+typedef struct s_pile
 {
 	int				value;
 	struct s_pile	*prev;
@@ -27,53 +27,54 @@ typedef	struct	s_pile
 typedef struct s_mq
 {
 	int	med;
-	int q1;
+	int	q1;
 	int	q2;
-	int len;
+	int	len;
 }		t_mq;
 
 // src
-size_t	ft_strlen(const char *s);
-void	quick_sort(int *array, int low, int high);
+size_t		ft_strlen(const char *s);
+void		quick_sort(int *array, int low, int high);
 long long	ft_atoi(const char *nptr);
-char	**ft_split(char const *s, char c);
-char	*ft_strdup(const char *src);
-int	ft_atoi_and_check(const char *nptr, int *error);
+char		**ft_split(char const *s, char c);
+char		*ft_strdup(const char *src);
+int			ft_atoi_and_check(const char *nptr, int *error);
+void		fill_mq(t_mq **mq, int len, int array[]);
 
 // utils
-void	free_pile(t_pile **pile);
-void	free_all(t_pile **pile_a, t_pile **pile_b, t_pile **mq);
-int	new_element(int nbr, t_pile **element);
-int	len(t_pile *pile);
+void		free_pile(t_pile **pile);
+void		free_all(t_pile **pile_a, t_pile **pile_b, t_pile **mq);
+int			new_element(int nbr, t_pile **element);
+int			len(t_pile *pile);
 
 // push
-void	push_b(t_pile **pilea, t_pile **pileb, t_mq **mq);
-void	push_b(t_pile **pilea, t_pile **pileb, t_mq **mq);
+void		push_b(t_pile **pilea, t_pile **pileb, t_mq **mq);
+void		push_b(t_pile **pilea, t_pile **pileb, t_mq **mq);
 
 // swap
-void	swap_a(t_pile **pilea);
-void	swap_b(t_pile **pileb);
+void		swap_a(t_pile **pilea);
+void		swap_b(t_pile **pileb);
 
 // rotate
-void	rotate_a(t_pile **pile_a, int print);
-void	rotate_b(t_pile **pile_b, int print);
-void	rotate_all(t_pile **pilea, t_pile **pileb);
+void		rotate_a(t_pile **pile_a, int print);
+void		rotate_b(t_pile **pile_b, int print);
+void		rotate_all(t_pile **pilea, t_pile **pileb);
 
 // reverse_rotate
-void	reverse_rotate_a(t_pile **pile_a, int print);
-void	reverse_rotate_b(t_pile **pile_b, int print);
-void	inverse_rotate_all(t_pile **pilea, t_pile **pileb);
+void		reverse_rotate_a(t_pile **pile_a, int print);
+void		reverse_rotate_b(t_pile **pile_b, int print);
+void		inverse_rotate_all(t_pile **pilea, t_pile **pileb);
 
 // get things
-int	get_index(t_pile *pile, int elem);
-int get_min(t_pile *pile);
-int get_max(t_pile *pile);
-int	get_pos_to_place(t_pile *pile, int elem);
-int	get_best_move(t_pile *pile_a, t_pile *pile_b);
+int			get_index(t_pile *pile, int elem);
+int			get_min(t_pile *pile);
+int			get_max(t_pile *pile);
+int			get_pos_to_place(t_pile *pile, int elem);
+int			get_best_move(t_pile *pile_a, t_pile *pile_b);
 
 // algo
-void	sort_three(t_pile **pile_a);
-int	is_sorted(t_pile *pile);
-int	inner(t_pile *pile, int q1, int q2);
+void		sort_three(t_pile **pile_a);
+int			is_sorted(t_pile *pile);
+int			inner(t_pile *pile, int q1, int q2);
 
 #endif
